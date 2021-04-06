c2a8687832d2613d7da39e94a3c82206618459fc
jacksonfrontend <evgenij_poposemov@mail.ru>
2021-04-06 22:52:32 +0300

    commit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,20 +1,5 @@
-function deleteDigit(n) {
-  const arr = [];
-  let num = n;
-  while (n > 0) { arr.unshift(n % 10); num = num / 10 || 0; }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-      arr.splice(i, 1);
-      return Number.parseInt(arr.join(''), 10);
-    }
-  }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 1) {
-      arr.splice(i, 1);
-      return Number.parseInt(arr.join(''), 10);
-    }
-  }
-  return arr;
+function deleteDigit(/* n */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = deleteDigit;
--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,14 +1,5 @@
-function getSumOfDigits(n) {
-  let res = 0;
-  let num = n;
-  const arr = [];
-  while (n > 0) { arr.unshift(n % 10); num = num / 10 || 0; }
-  // eslint-disable-next-line no-return-assign
-  arr.forEach((elem) => res += elem);
-  if (res > 9) {
-    res -= 9;
-  }
-  return res;
+function getSumOfDigits(/* n */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = getSumOfDigits;
