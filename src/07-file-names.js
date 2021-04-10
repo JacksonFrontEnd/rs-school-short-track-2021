14efecdbe62c427427c8cabbd6d89f90c5dd83a5
JacksonFrontEnd <evgenij_poposemov@mail.ru>
2021-04-10 18:56:17 +0300

    commit

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -12,17 +12,3 @@ function renameFiles(names) {
   return mas;
 }
 module.exports = renameFiles;
-function renameFiles1(names) {
-  let temp;
-  const mas = names;
-  for (let i = 0; i < mas.length; i++) {
-    temp = mas[i];
-    for (let j = i + 1; j < mas.length; j++) {
-      if (temp === mas[j]) {
-        mas[j] = `${mas[j]}(${i + 1})`;
-      }
-    }
-  }
-  return mas;
-}
-console.log(renameFiles1('doc', 'doc', 'image', 'doc(1)', 'doc'));
